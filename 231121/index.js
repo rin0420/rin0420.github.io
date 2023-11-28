f287b8f60ff6b7868ad064f7cdc77792cbf6f4dd
m0a22305f1 <rinrin140420@gmail.com>
2023-11-28 22:12:15 +0900

    2023春学期

--- 231121/index.js
+++ 231121/index.js
@@ -1,9 +1,32 @@
-let answer = document.querySelector('#answer');
-let outputHtml = '<ul>';
-
-for (let tweet of tweets) {
-    outputHtml += `<li><img src="${tweet.avatar}" width="50" height="50" alt="${tweet.name}のプロフィール画像"></img><a>${tweet.name}</a>: ${tweet.message} <i>${tweet.tweetedAt}</i></li>`;
+function outputTweets(tweets) {
+    let outputHtml = '<ul>';
+    for (let tweet of tweets) {
+        outputHtml += `<li><img src="${tweet.avatar}" width="50" height="60" alt="${tweet.name}のプロフィール画像"></img><a>${tweet.name}</a>: ${tweet.message} <i>${tweet.tweetedAt}</i></li>`;
+    }
+    outputHtml += '</ul>';
+    answer.innerHTML = outputHtml;
 }
 
-outputHtml += '</ul>';
-answer.innerHTML = outputHtml;
+window.onload = function() {
+    outputTweets(tweets);
+};
+
+function allButton() {
+    outputTweets(tweets);
+}
+
+function saburonBotButton() {
+    outputTweets(tweets.filter(tweet => tweet.name === "三郎BOT"));
+}
+function jiroButton() {
+    outputTweets(tweets.filter(tweet => tweet.name === "次郎"));
+}
+
+function taroButton() {
+    outputTweets(tweets.filter(tweet => tweet.name === "太郎"));
+}
+
+
+
+
+
